f81210e2b46356e64d3eaec5d15d219d28cc4474
Jaime García Villena <garciavillena.jaime@gmail.com>
2017-02-20 22:24:28 +0100

    Change ino extension into cpp

--- src/ESP8266_SoftAP_Server.cpp
+++ src/ESP8266_SoftAP_Server.cpp
@@ -14,14 +14,48 @@ const int DIGITAL_PIN = 12; // Digital pin to be read
 
 WiFiServer server(80);
 
-void setup() 
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+
+  // Do a little work to get a unique-ish name. Append the
+  // last two bytes of the MAC (HEX'd) to "Thing-":
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+  String AP_NameString = "ESP8266 Thing " + macID;
+
+  char AP_NameChar[AP_NameString.length() + 1];
+  memset(AP_NameChar, AP_NameString.length() + 1, 0);
+
+  for (int i=0; i<AP_NameString.length(); i++)
+    AP_NameChar[i] = AP_NameString.charAt(i);
+
+  WiFi.softAP(AP_NameChar, WiFiAPPSK);
+}
+
+void initHardware()
+{
+  Serial.begin(115200);
+  pinMode(DIGITAL_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  // Don't need to set ANALOG_PIN as input,
+  // that's all it can be.
+}
+
+
+void setup()
 {
   initHardware();
   setupWiFi();
   server.begin();
 }
 
-void loop() 
+void loop()
 {
   // Check if a client has connected
   WiFiClient client = server.available();
@@ -33,7 +67,7 @@ void loop()
   String req = client.readStringUntil('\r');
   Serial.println(req);
   client.flush();
-  
+
   // Match the request
   int val = -1; // We'll use 'val' to keep track of both the
                 // request type (read/set) and value if set.
@@ -48,7 +82,7 @@ void loop()
   // Set GPIO5 according to the request
   if (val >= 0)
     digitalWrite(LED_PIN, val);
-  
+
   client.flush();
 
   // Prepare the response. Start with the common header:
@@ -80,38 +114,6 @@ void loop()
   delay(1);
   Serial.println("Client disonnected");
 
-  // The client will actually be disconnected 
+  // The client will actually be disconnected
   // when the function returns and 'client' object is detroyed
-}
-
-void setupWiFi()
-{
-  WiFi.mode(WIFI_AP);
-  
-  // Do a little work to get a unique-ish name. Append the
-  // last two bytes of the MAC (HEX'd) to "Thing-":
-  uint8_t mac[WL_MAC_ADDR_LENGTH];
-  WiFi.softAPmacAddress(mac);
-  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
-                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
-  macID.toUpperCase();
-  String AP_NameString = "ESP8266 Thing " + macID;
-  
-  char AP_NameChar[AP_NameString.length() + 1];
-  memset(AP_NameChar, AP_NameString.length() + 1, 0);
-  
-  for (int i=0; i<AP_NameString.length(); i++)
-    AP_NameChar[i] = AP_NameString.charAt(i);
-  
-  WiFi.softAP(AP_NameChar, WiFiAPPSK);
-}
-
-void initHardware()
-{
-  Serial.begin(115200);
-  pinMode(DIGITAL_PIN, INPUT_PULLUP);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
-  // Don't need to set ANALOG_PIN as input, 
-  // that's all it can be.
 }
