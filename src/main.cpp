7212a9a36d2f8cde123a3366731dd5d972fe9b4c
Jaime García Villena <garciavillena.jaime@gmail.com>
2017-02-27 19:15:26 +0100

    Add flash size to platformio.ini. Make blink example on gpio 14

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,12 @@
 #include <ESP8266WiFi.h>
 
 void setup(){
-  pinMode(4,OUTPUT);
-  digitalWrite(4,LOW);
-  Serial.begin(9600);
-  delay(2000);
-  Serial.print("Serial test 1\r\n");
-  delay(2000);
-  Serial.print("Serial test 2\r\n");
-  delay(2000);
-  digitalWrite(4,HIGH);
+  pinMode(14,OUTPUT);
 }
 
 void loop(){
-
+  digitalWrite(14,HIGH);
+  delay(1000);
+  digitalWrite(14,LOW);
+  delay(1000);
 }
