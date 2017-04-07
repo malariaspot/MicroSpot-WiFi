c16ff759f1cb7c1e5b881107e1c10d95c8755206
alex <alex@malariaspot.org>
2017-04-07 13:23:03 +0200

    Server code changes + main.ino cleaned up

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,81 +2,55 @@
  * Project Untitled
  */
 
-
 #include "Server.h"
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
 
 /**
  * Server implementation
  */
 
-Server::Server() {
-	serverWifi(80);
-}
+Server::Server() {}
 
-void Server::setUp() {
-	String APname(HOSTNAME);
-  	APname += String(ESP.getChipId(),HEX);
-  	WiFi.hostname(hostname);
+void Server::setUp(String hostname) {
+ 	WiFi.hostname(hostname);
 
-  	// Check WiFi connection
+  // Check WiFi connection
   // ... check mode
-  if (WiFi.getMode() != WIFI_STA)
-  {
+  if (WiFi.getMode() != WIFI_STA) {
     WiFi.mode(WIFI_STA);
     delay(10);
   }
 
   // ... Compare file config with sdk config.
-  if (WiFi.SSID() != station_ssid || WiFi.psk() != station_psk)
-  {
+  if (WiFi.SSID() != station_ssid || WiFi.psk() != station_psk) {
 
     WiFi.begin(station_ssid.c_str(), station_psk.c_str());
 
-  }
-  else
-  {
+  }else{
     // ... Begin with sdk config.
     WiFi.begin();
   }
-
-  //Serial.println("Wait for WiFi connection.");
-
   // ... Give ESP 10 seconds to connect to station.
   unsigned long startTime = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - startTime < 10000)
-  {
-    //Serial.write('.');
-    ////Serial.print(WiFi.status());
-    delay(500);
-  }
-  //Serial.println();
+  while (WiFi.status() != WL_CONNECTED && millis() - startTime < 10000) { delay(500); }
 
   // Check connection
-  if(WiFi.status() == WL_CONNECTED)
-  {
-    // ... print IP Address
-    //Serial.print("IP address: ");
-    //Serial.println(WiFi.localIP());
-  }
-  else
-  {
-    //Serial.println("Can not connect to WiFi station. Go into AP mode.");
-
+  if(WiFi.status() != WL_CONNECTED) {
     // Go into software AP mode.
     WiFi.mode(WIFI_AP);
 
     delay(10);
 
     WiFi.softAP((const char *)APname.c_str(), ap_default_psk);
-
-    //Serial.print("IP address: ");
-    //Serial.println(WiFi.softAPIP());
   }
+
+  serverWifi.begin();
 }
 
 void Server::run() {
-	 // Check if a client has connected
+	// Check if a client has connected
   WiFiClient client = server.available();
   if (!client) { return; }
   // Wait until the client sends some data
@@ -96,13 +70,8 @@ void Server::run() {
   client.println(prepareHtmlPage(val));
 }
 
-void Server::success() {
-
-}
-
-void Server::error() {
-
-}
+void Server::success() { client.println(prepareHtmlPage("Done")); }
+void Server::error() { client.println(prepareHtmlPage("Error")); }
 
 String Server::prepareHtmlPage(String response) {
   String htmlPage = String("HTTP/1.1 200 OK\r\n") +
--- src/main.ino
+++ src/main.ino
@@ -13,14 +13,14 @@
  */
 
 // includes
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
+//#include <ESP8266WiFi.h>
+//#include <ESP8266mDNS.h>
+//#include <WiFiUdp.h>
 #include <FS.h>
 #include <ArduinoOTA.h>
 #include <Ticker.h>
 #include "Mechanical.h"
-#include "Mechanical.h"
+#include "Server.h"
 
 ///////////////////////////////////////////////
 // GPIO definitions
@@ -29,7 +29,6 @@
 #define LEDPIN 14 //GPIO for the LED
 #define SERIALEN 4 //GPIO to activate Arduino-esp8266 serial bridge (LOW == ON)
 
-
 /**
  * @brief mDNS and OTA Constants
  * @{
@@ -37,7 +36,6 @@
 #define HOSTNAME "MicroSpot-" ///< Hostename. The setup function adds the Chip ID at the end.
 //Also used for AP name.
 /// @}
-
 
 //WiFiServer server(80);
 Server server();
@@ -59,8 +57,6 @@ Ticker ledBlink;
 void ledFlick(){
   digitalWrite(LEDPIN,!digitalRead(LEDPIN));
 }
-
-
 
 /**
  * @brief Read WiFi connection information from file system.
@@ -93,19 +89,16 @@ bool loadConfig(String *ssid, String *pass)
   int8_t pos = content.indexOf("\r\n");
   uint8_t le = 2;
   // check for linux and mac line ending.
-  if (pos == -1)
-  {
+  if (pos == -1) {
     le = 1;
     pos = content.indexOf("\n");
-    if (pos == -1)
-    {
+    if (pos == -1) {
       pos = content.indexOf("\r");
     }
   }
 
   // If there is no second line: Some information is missing.
-  if (pos == -1)
-  {
+  if (pos == -1) {
     //Serial.println("Invalid content.");
     //Serial.println(content);
     return false;
@@ -121,22 +114,16 @@ bool loadConfig(String *ssid, String *pass)
   return true;
 }
 
-
 /**
  * @brief Save WiFi SSID and PSK to configuration file.
  * @param ssid SSID as string pointer.
  * @param pass PSK as string pointer,
  * @return True or False.
  */
-bool saveConfig(String *ssid, String *pass)
-{
+bool saveConfig(String *ssid, String *pass) {
   // Open config file for writing.
   File configFile = SPIFFS.open("/cl_conf.txt", "w");
-  if (!configFile)
-  {
-    //Serial.println("Failed to open cl_conf.txt for writing");
-    return false;
-  }
+  if (!configFile) { return false; }
   // Save SSID and PSK.
   configFile.println(*ssid);
   configFile.println(*pass);
@@ -144,12 +131,10 @@ bool saveConfig(String *ssid, String *pass)
   return true;
 }
 
-
 /**
  * @brief Arduino setup function.
  */
-void setup()
-{
+void setup() { 
   String station_ssid = "";
   String station_psk = "";
 
@@ -160,146 +145,37 @@ void setup()
 
   delay(100);
 
-
   // Set Hostname.
   String hostname(HOSTNAME);
   hostname += String(ESP.getChipId(), HEX);
 
-  /*String APname(HOSTNAME);
+  String APname(HOSTNAME);
   APname += String(ESP.getChipId(),HEX);
-  WiFi.hostname(hostname);*/
-
-
-
-
   // Initialize file system.
-  if (!SPIFFS.begin())
-  {
-    //html message
-    //Serial.println("Failed to mount file system");
-    return;
-  }
-
+  if (!SPIFFS.begin()) { return; }
   // Load wifi connection information.
-  if (! loadConfig(&station_ssid, &station_psk))
-  {
+  if (! loadConfig(&station_ssid, &station_psk)) {
     station_ssid = "";
     station_psk = "";
-    //Serial.println("No WiFi connection information available.");
   }
-
-  /*// Check WiFi connection
-  // ... check mode
-  if (WiFi.getMode() != WIFI_STA)
-  {
-    WiFi.mode(WIFI_STA);
-    delay(10);
-  }
-
-  // ... Compare file config with sdk config.
-  if (WiFi.SSID() != station_ssid || WiFi.psk() != station_psk)
-  {
-
-    WiFi.begin(station_ssid.c_str(), station_psk.c_str());
-
-  }
-  else
-  {
-    // ... Begin with sdk config.
-    WiFi.begin();
-  }
-
-  //Serial.println("Wait for WiFi connection.");
-
-  // ... Give ESP 10 seconds to connect to station.
-  unsigned long startTime = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - startTime < 10000)
-  {
-    //Serial.write('.');
-    ////Serial.print(WiFi.status());
-    delay(500);
-  }
-  //Serial.println();
-
-  // Check connection
-  if(WiFi.status() == WL_CONNECTED)
-  {
-    // ... print IP Address
-    //Serial.print("IP address: ");
-    //Serial.println(WiFi.localIP());
-  }
-  else
-  {
-    //Serial.println("Can not connect to WiFi station. Go into AP mode.");
-
-    // Go into software AP mode.
-    WiFi.mode(WIFI_AP);
-
-    delay(10);
-
-    WiFi.softAP((const char *)APname.c_str(), ap_default_psk);
-
-    //Serial.print("IP address: ");
-    //Serial.println(WiFi.softAPIP());
-  }
-  */
-
-  server.setUp();
 
   ledBlink.attach(1,ledFlick);
 
   // Start OTA server.
   ArduinoOTA.setHostname((const char *)hostname.c_str());
   ArduinoOTA.begin();
-  server.begin();
-}
 
+  server.setUp();
+}
 
 /**
  * @brief Arduino loop function.
  */
-void loop()
-{
+void loop() {
   //serverLoop();
+  server.run();
   yield();
   // Handle OTA server.
   ArduinoOTA.handle();
   yield();
 }
-
-
-// prepare a web page to be send to a client (web browser)
-/*  String htmlPage = String("HTTP/1.1 200 OK\r\n") +
-            "Content-Type: text/html\r\n" +
-            "Connection: close\r\n" +  // the connection will be closed after completion of the response
-            "Refresh: 5\r\n" +  // refresh the page automatically every 5 sec
-            "\r\n" +
-            "<!DOCTYPE HTML>" +
-            "<html>" +
-            response +
-            "</html>" +
-            "\r\n";
-
-  return htmlPage;
-}
-
-void serverLoop() {
-  // Check if a client has connected
-  WiFiClient client = server.available();
-  if (!client) { return; }
-  // Wait until the client sends some data
-  while(!client.available()){ delay(1); }
-  // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  client.flush();
-  // Match the request
-  String val;
-  if (req.indexOf("/ayy/lmao") != -1) val = req;
-  else {
-    client.stop();
-    return;
-  }
-  client.flush();
-  // Prepare the response
-  client.println(prepareHtmlPage(val));
-}*/
