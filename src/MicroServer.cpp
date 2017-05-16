83546c4ffc807f11a623632dcae4d1f01b53b4cd
Jaime García Villena <garciavillena.jaime@gmail.com>
2017-05-16 17:02:00 +0200

    Fix parsing error that affected moveAxis and jogAxis. Issue #14

--- src/MicroServer.cpp
+++ src/MicroServer.cpp
@@ -117,26 +117,28 @@ void MicroServer::handleClients() {
       mechanical->getPos();
       return;
     }else if (req.indexOf("/moveAxis") != -1) {
-      int x,y,f;
+      int x,y,f,e;
       x = req.indexOf("x=");
       y = req.indexOf("y=");
       f = req.indexOf("f=");
+      e = req.indexOf(" ",f);
       if (x > 0 && y > 0 && f > 0) { 
-        if (mechanical->moveAxis(req.substring(x+2, y-1), req.substring(y+2, f - 1), req.substring(f+2))) {
+        if (mechanical->moveAxis(req.substring(x+2, y-1), req.substring(y+2, f - 1), req.substring(f+2,e))) {
           currentClient = client;
         }else{
           update("Busy", &client);
         }
       }else{ update("Error: One or more position arguments are missing!", &client); }
     }else if (req.indexOf("/jogAxis") != -1) {
-      int x,y,f,s, r;
+      int x,y,f,s,r,e;
       x = req.indexOf("x=");
       y = req.indexOf("y=");
       f = req.indexOf("f=");
       r = req.indexOf("r=");
       s = req.indexOf("s=");
+      e = req.indexOf(" ",s);
       if (x > 0 && y > 0 && f > 0) { 
-        if (mechanical->jogAxis(req.substring(x+2, y - 1),req.substring(y+2,f - 1),req.substring(f+2,r - 1),req.substring(r+2,s - 1),req.substring(s+2))) {
+        if (mechanical->jogAxis(req.substring(x+2, y - 1),req.substring(y+2,f - 1),req.substring(f+2,r - 1),req.substring(r+2,s - 1),req.substring(s+2, e))) {
           currentClient = client;
         }else{
           update("Busy", &client);
