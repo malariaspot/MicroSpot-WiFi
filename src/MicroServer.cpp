d15a491f363ee61f9f18682021f09c9ff824058f
alexx666 <alexander.vladimirov3@gmail.com>
2017-05-19 14:27:20 +0200

    param handling and url parsing

--- src/MicroServer.cpp
+++ src/MicroServer.cpp
@@ -2,54 +2,41 @@
 #include "Mechanical.h"
 #include <Ticker.h>
 
-WiFiServer serverWifi(80);
-
-///////////////////////////////////////////////
-// LED ticker and functions to make a Blink
-///////////////////////////////////////////////
-
 #define LEDPIN 14 //GPIO for the LED
 
-Ticker ledBlink;
+Ticker ledBlink; // LED ticker and functions to make a Blink
 
-void ledFlick(){
-  digitalWrite(LEDPIN,!digitalRead(LEDPIN));
-}
+void ledFlick() { digitalWrite(LEDPIN,!digitalRead(LEDPIN)); }
 
-/**
- * Server implementation
- */
+WiFiServer serverWifi(80);
+
+/* INIT */
 
 MicroServer::MicroServer(Mechanical *m) {
   mechanical = m;
   mechanical->addObserver(this);
 }
 
-void MicroServer::setup(String hostname) {
+/* PUBLIC */
 
+void MicroServer::setup(String hostname) {
   //Set the hostname of the server
   WiFi.hostname(hostname);
-
   //Check of there has been a change in WiFi configuration.
   String station_ssid, station_psk;
-
-    // Load wifi connection information.
+  // Load wifi connection information.
   if (! fileManager.loadWifiConfig(&station_ssid, &station_psk)) {
     station_ssid = "";
     station_psk = "";
   }
-
   // Check WiFi connection
   if (WiFi.getMode() != WIFI_STA) {
     WiFi.mode(WIFI_STA);
     delay(10);
   }
-
   // ... Compare file config with sdk config.
   if (WiFi.SSID() != station_ssid || WiFi.psk() != station_psk) {
-
     WiFi.begin(station_ssid.c_str(), station_psk.c_str());
-
   }else{
     // ... Begin with sdk config.
     WiFi.begin();
@@ -57,7 +44,6 @@ void MicroServer::setup(String hostname) {
   // ... Give ESP 10 seconds to connect to station.
   unsigned long startTime = millis();
   while (WiFi.status() != WL_CONNECTED && millis() - startTime < 10000) { delay(500); }
-
   // Check connection
   if(WiFi.status() != WL_CONNECTED) {
     // Go into software AP mode.
@@ -79,78 +65,88 @@ void MicroServer::setup(String hostname) {
 
 void MicroServer::run() {
   newClient = serverWifi.available();
-
   if (newClient) {
     while(!newClient.available()) { delay(1); }
+    request = newClient.readStringUntil('\r');
     
-    parseRequest();
+    if (request.indexOf('?') != -1) {
+      url = request.substring(request.indexOf("GET ")+4, request.indexOf("?"));
+    }else{
+      url = request.substring(request.indexOf("GET ")+4, request.indexOf(" HTTP/"));      
+    }
+    
     handleClient();
   }
 }
 
-void MicroServer::parseRequest() {
-  url = newClient.readStringUntil('\r');
-  newClient.flush();
+void MicroServer::update(String msg) { send(msg, &currentClient); }
+
+/* PRIVATE */
+
+String MicroServer::arg(String arg) {
+  int end;
+  if (request.indexOf(" ",request.indexOf(arg + "="))) {
+    end = request.indexOf(" ",request.indexOf(arg + "="));
+  }else if (request.indexOf("&",request.indexOf(arg + "="))) {
+    end = request.indexOf("&",request.indexOf(arg + "="));
+  }
+  return request.substring(request.indexOf(arg+"=")+2, end);
+}
+
+bool MicroServer::hasArg(String arg) {
+  if (request.indexOf(arg+"=") != -1) return true;
+  return false;
 }
 
 void MicroServer::handleClient() {
-  if (url.indexOf("/ayy/lmao") != -1) {
-      send("Ayy Lmao", &newClient);
-    }else if (url.indexOf("/homeAxis") != -1) {
-      if (mechanical->homeAxis()) {
-        currentClient = newClient;
-      }else{
-        send("Busy", &newClient);
-      }
-    }else if (url.indexOf("/stopJog") != -1) {
-      if (mechanical->stopJog()) {
-        currentClient = newClient;
-      }else{
-        send("Busy", &newClient);
-      }
-    }else if (url.indexOf("/getPos") != -1) {
-      currentClient = newClient;
-      mechanical->getPos();
-    }else if (url.indexOf("/moveAxis") != -1) {
-      int x,y,f,e;
-      x = url.indexOf("x=");
-      y = url.indexOf("y=");
-      f = url.indexOf("f=");
-      e = url.indexOf(" ",f);
-      if (x > 0 && y > 0 && f > 0) { 
-        if (mechanical->moveAxis(url.substring(x+2, y-1), url.substring(y+2, f - 1), url.substring(f+2,e))) {
-          currentClient = newClient;
-        }else{
-          send("Busy", &newClient);
-        }
-      }else{ send("Error: One or more position arguments are missing!", &newClient); }
-    }else if (url.indexOf("/jogAxis") != -1) {
-      int x,y,f,s,r,e;
-      x = url.indexOf("x=");
-      y = url.indexOf("y=");
-      f = url.indexOf("f=");
-      r = url.indexOf("r=");
-      s = url.indexOf("s=");
-      e = url.indexOf(" ",s);
-      if (x > 0 && y > 0 && f > 0) { 
-        if (mechanical->jogAxis(url.substring(x+2, y - 1),url.substring(y+2,f - 1),url.substring(f+2,r - 1),url.substring(r+2,s - 1),url.substring(s+2, e))) {
-          currentClient = newClient;
-        }else{
-          send("Busy", &newClient);
-        }
-      }else{ send("Error: One or more position arguments are missing!", &newClient); }
-    }else{
-      send("Not found!", &newClient);
-      return;
-    }
+  newClient.flush();
+
+  if (url == "/ayy/lmao") {
+    send("Ayy Lmao", &newClient);
+  }else if (url == "/home") {
+
+    if (mechanical->homeAxis()) { currentClient = newClient; }
+    else{ send("Busy", &newClient); }
+
+  }else if (url == "/stop") {
+
+    if (mechanical->stopJog()) { currentClient = newClient; }
+    else{ send("Busy", &newClient); }
+
+  }else if (url == "/position") {
+
+    currentClient = newClient;
+    mechanical->getPos();
+
+  }else if (url == "/move") {
+
+    if (hasArg("x") && hasArg("y") && hasArg("f")) { 
+
+      if (mechanical->moveAxis(arg("x"),arg("y"),arg("f"))) { 
+        currentClient = newClient; 
+      }else{ send("Busy", &newClient); }
+
+    }else{ send("Error: One or more position arguments are missing!", &newClient); }
+
+  }else if (url == "/jog") {
+
+    if (hasArg("x") && hasArg("y") && hasArg("f") && hasArg("r") && hasArg("s")) {
+
+      if (mechanical->jogAxis(arg("x"),arg("y"),arg("f"),arg("r"),arg("s"))) { 
+        currentClient = newClient; 
+      }else{ send("Busy", &newClient); }
+
+    }else{ send("Error: One or more position arguments are missing!", &newClient); }
+
+  }else{ send(url + " not found!", &newClient); }
+  //return;
 }
 
-void MicroServer::update(String msg) { send(msg, &currentClient); }
 void MicroServer::send(String msg, WiFiClient * client) { 
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+    + msg + "\r\n";
+
   client->flush();
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nSuccess: "+msg+"\r\n" 
-   + client->remoteIP().toString() + ":" + client->remotePort() + " " 
-   + client->localIP().toString() + ":" + client->localPort();
   client->print(s);
   delay(1);
   client->stop();
--- src/MicroServer.h
+++ src/MicroServer.h
@@ -6,37 +6,33 @@
 #include "WiFiServer.h"
 #include <WiFiClient.h>
 #include <ESP8266WiFi.h>
-//#include <ESP8266WebServer.h> 
-
-#include <string>
-#include <tuple>
-
 #include "FileManager.h"
 
-class Mechanical;
+class Mechanical; //Defined and imported in MicroServer.cpp
 
 class MicroServer {
 
 	public: 
-		MicroServer(Mechanical *m);
 
-		void run();
-		void setup(String hostname);
-		void update(String msg);
-		void send(String msg, WiFiClient * client);
+		MicroServer(Mechanical * m); //Constructor
+		void run(); //Loop
+		void setup(String hostname); //init
+		void update(String msg); //Event handler
+
 	private: 
+
 		const char* ap_default_psk = "microspot"; ///< Default PSK.
-
-		WiFiClient currentClient;
-		WiFiClient newClient;
-		String url;
-		String method;
-
-		FileManager fileManager;
-		Mechanical *mechanical;
-
-		void parseRequest();
-		void handleClient();
+		Mechanical *mechanical; //Mechanical object
+		FileManager fileManager; //Config file manager
+		WiFiClient currentClient; //client for mechanical
+		WiFiClient newClient; //CASUL client
+		String request; //Request sent
+		String url; //URL segment of the request
+		String arg(String arg); //Returns requested argument
+		bool hasArg(String arg); //Checks if requested argument exists
+		void handleClient(); //handles the client if he exists
+		void send(String msg, WiFiClient * client); //Sends a responce message to a specific client
+		
 };
 
 #endif //MICROSERVER_H
