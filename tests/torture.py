319a7b91f2a38f607d8f30f1422b349b2be805f8
Jaime García Villena <garciavillena.jaime@gmail.com>
2017-05-29 12:51:25 +0200

    Make a more automated torture test

--- tests/torture.py
+++ tests/torture.py
@@ -2,7 +2,7 @@ import urllib.request as urllib2
 import time
 import math
 
-frequency = 0.2
+frequency = 0.15
 count = 0
 
 class CircleIterator:
@@ -12,12 +12,20 @@ class CircleIterator:
         return self
     def next(self):
         self.angle = self.angle + 5
-        return [0.1*math.cos(self.angle*math.pi/180),0.1*math.sin(self.angle*math.pi/180)]
+        return [0.2*math.cos(self.angle*math.pi/180),0.2*math.sin(self.angle*math.pi/180)]
 
 c = CircleIterator()
 timestamp = time.time()
 speed = 6/frequency
 f = '%.3f' % speed
+
+try:
+    print(urllib2.urlopen("http://192.168.4.1/ayy/lmao").read())
+    print(urllib2.urlopen("http://192.168.4.1/home").read())
+    print(urllib2.urlopen("http://192.168.4.1/move?x=25&y=7.5&f=500").read())
+except:
+    print("MicroSpot is dead, or disconnected")
+    exit()
 
 while True:
     if time.time() - timestamp > frequency:
@@ -26,5 +34,13 @@ while True:
         list = c.next()
         x = '%.3f' % list[0]
         y = '%.3f' % list[1]
-        print(urllib2.urlopen("http://192.168.4.1/pan?x=" + x + "&y=" + y + "&f=" + f).read())
-        print(count)
+        try:
+            before = time.time()
+            print(urllib2.urlopen("http://192.168.4.1/pan?x=" + x + "&y=" + y + "&f=" + f).read())
+            after = time.time()
+        except Exception as inst:
+            print(inst)
+            break;
+        print(str(count) + " delay: " + str(after - before))
+
+print("MicroSpot died after " + str(count) + " requests, at a rate of " + str(frequency) + " seconds per petition")
